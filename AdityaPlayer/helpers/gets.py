2180333840067596ba0ca4e6103b8270346d1ad8
User786k <90415625+User786k@users.noreply.github.com>
2021-09-10 00:10:41 +0530

    Update gets.py

--- AdityaPlayer/helpers/gets.py
+++ AdityaPlayer/helpers/gets.py
@@ -1,5 +1,5 @@
-# Aditya Player - Telegram bot for streaming audio in group calls
-# Copyright (C) 2021  Aditya Halder
+# BADSHAH PLAYER - Telegram bot for streaming audio in group calls
+# Copyright (C) 2021  BADSHAH SHAM
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
