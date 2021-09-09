d11d00bd33fbf01931d8d5e5110570444f589242
User786k <90415625+User786k@users.noreply.github.com>
2021-09-10 00:24:19 +0530

    Update msg.py

--- AdityaPlayer/modules/msg.py
+++ AdityaPlayer/modules/msg.py
@@ -1,5 +1,5 @@
-# AdityaPlayer (Telegram bot project )
-# Copyright (C) 2021  Aditya Halder
+# BADSHAH PLAYER(Telegram bot project )
+# Copyright (C) 2021  BADSHAH SHAM
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
